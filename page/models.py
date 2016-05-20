e62bd1172363fdb487986a28c26a608e92a4b855
Rita Liang <viso.liang@qq.com>
2016-05-20 18:31:05 +0800

    修改用户liang的密码，添加app_name的中文支持(未成功)

--- page/models.py
+++ page/models.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 
+class string_with_title(str):
+    def __new__(cls, value, title):
+        instance = str.__new__(cls, value)
+        instance._title = title
+        return instance
+
+    def title(self):
+        return self._title
+
+    __copy__ = lambda self: self
+    __deepcopy__ = lambda self, memodict: self
 
 class Catalog(models.Model):
     name = models.CharField(u"目录名称", max_length = 30)
@@ -8,6 +19,7 @@ class Catalog(models.Model):
     class Meta:
         verbose_name_plural = u"目录"
         verbose_name = u"目录"
+        app_label = string_with_title('page', u"网站管理")
 
 class Article(models.Model):
     catelog = models.ForeignKey(Catalog, verbose_name=u"所属目录")
@@ -18,6 +30,7 @@ class Article(models.Model):
     class Meta:
         verbose_name = u"文章"
         verbose_name_plural = u"文章"
+        app_label = string_with_title('page', u"网站管理")
 
     def __str__(self):
         return self.name
@@ -31,3 +44,4 @@ class Image(models.Model):
     class Meta:
         verbose_name = u"图片"
         verbose_name_plural = u"图片"
+        app_label = string_with_title('page', u"网站管理")
