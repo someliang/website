04b465a8839c0280dbbd864bf956f71913994de3
Rita Liang <viso.liang@qq.com>
2016-05-21 09:49:54 +0800

    1.添加路径文件page/urls 2.将page的路径文件加入项目urls 3.将静态首页加入项目，成为动态页面。

--- page/views.py
+++ page/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from models import Catalog
 
-# Create your views here.
+def index(request):
+    catalogs = Catalog.objects.all()
+    context = {
+        'catalogs' : catalogs
+    }
+    return render(request, 'index.html', context)
