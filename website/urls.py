36ca39a85e13757aca5c897360efda4ff7966406
Rita Liang <viso.liang@qq.com>
2016-05-21 09:12:11 +0800

    1.删除没有用的路径映射文件。 2.添加网站需要的静态文件

--- website/urls.py
+++ website/urls.py
@@ -21,5 +21,4 @@ pic_path = '/home/liang/projects/website/website/media'
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^summernote/', include('django_summernote.urls')),
-    url(r'^static/(?P<path>.*)', 'django.views.static.serve', {'document_root': pic_path, 'show_indexes':True}),
 ]
